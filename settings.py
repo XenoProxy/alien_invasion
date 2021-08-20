959fe0603977367aeb9a5ff90fc7407b27296c59
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2021-08-20 12:30:09 +0300

    Bullet settings

--- settings.py
+++ settings.py
@@ -8,3 +8,8 @@ class Settings:
         self.bg_color = (255, 255, 255)
         # Настройки корабля
         self.ship_speed = 0.5
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
