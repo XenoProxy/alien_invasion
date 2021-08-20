e70e2e866713118f1eedae9cbf116d3ae457202d
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2021-08-20 11:55:33 +0300

    Float speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,15 +30,15 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:  # Переместить корабль вправо.
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:  # Переместить корабль влево.
+                if event.key == pygame.K_LEFT:  # Переместить корабль влево.
                     self.ship.moving_left = True
+                elif event.key == pygame.K_RIGHT:  # Переместить корабль вправо.
+                    self.ship.moving_right = True
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:  # Переместить корабль вправо.
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:  # Переместить корабль влево.
+                if event.key == pygame.K_LEFT:  # Переместить корабль влево.
                     self.ship.moving_left = False
+                elif event.key == pygame.K_RIGHT:  # Переместить корабль вправо.
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
